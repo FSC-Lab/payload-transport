fe135b5b0ad8389046eca4cba2156d23986d4f47
hs293go <hellston20a@gmail.com>
2021-08-04 00:29:08 +0800

    Let home position be passed in to computePath()
    Remove waypoint pulling from WaypointManager
    
    Let default velocity be passed in to getVelocities()
    
    Fold computeHeadings into computePath

--- include/payload_transport/WaypointManager.h
+++ include/payload_transport/WaypointManager.h
@@ -19,23 +19,16 @@ class WaypointManager {
 
   ros::NodeHandle nh_;
   ros::Subscriber wp_sub_;
-  ros::Subscriber gp_sub_;
-  ros::ServiceClient wp_client_;
   ros::ServiceClient param_client_;
 
   const std::string gp_sub_key_;
   const std::string wp_sub_key_;
 
   const std::string param_sub_key_;
-  const std::string wp_pull_key_;
-  const std::string wp_clear_key_;
 
   mavros_msgs::WaypointList wps_;
 
-  Eigen::Vector3d home_position_;
-
  public:
-  const Eigen::Vector3d& home_position() const { return home_position_; }
 
   const utils::StdVector_t<Eigen::Vector3d>& waypoints() { return waypoints_; }
 
@@ -44,59 +37,25 @@ class WaypointManager {
   auto waypointNum() { return waypoints_.size(); }
   auto waypointIndex() { return wp_index_; }
 
-  bool isHomePositionValid() { return !home_position_.isZero(1e-3); }
-
   WaypointManager(const std::string& base_name)
       : nh_("~"),
         wp_sub_key_(base_name + "/mission/waypoints"),
-        wp_pull_key_(base_name + "/mission/pull"),
-        gp_sub_key_(base_name + "/global_position/global"),
         param_sub_key_(base_name + "/param/get"),
-        home_position_(utils::zeros<double, 3>),
         wp_index_(0) {
     wp_sub_ = nh_.subscribe<mavros_msgs::WaypointList>(
         wp_sub_key_, 1, [&](const mavros_msgs::WaypointListConstPtr& msg) { wps_ = std::move(*msg); });
 
-    gp_sub_ = nh_.subscribe<sensor_msgs::NavSatFix>(gp_sub_key_, 1, [&](const sensor_msgs::NavSatFixConstPtr& msg) {
-      home_position_ << msg->latitude, msg->longitude, msg->altitude;
-    });
-
-    wp_client_ = nh_.serviceClient<mavros_msgs::WaypointPull>(wp_pull_key_);
     param_client_ = nh_.serviceClient<mavros_msgs::ParamGet>(param_sub_key_);
   };
 
-  bool getWaypoints() {
-    mavros_msgs::WaypointPull srv;
-    if (wp_client_.call(srv)) {
-      if (srv.response.success && srv.response.wp_received > 0) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  }
-
-  bool getVelocities() {
-    if (isHomePositionValid() && gcsWaypointNum() > 0) {
+  bool getVelocities(double backup_speed = 4.0) {
+    if (gcsWaypointNum() > 0) {
       if (nh_.getParam("/custom/nav/velocities", velocities_)) {
         if (velocities_.size() == gcsWaypointNum() - 1) {
           ROS_INFO("Got path-following velocities from parameters!");
           return true;
         }
-      } else {
-        mavros_msgs::ParamGet srv;
-        srv.request.param_id = "WPNAV_SPEED";
-        if (param_client_.call(srv)) {
-          if (srv.response.success) {
-            auto& wpnav_speed = srv.response.value.real;
-            velocities_.resize(gcsWaypointNum() - 1);
-            std::fill(velocities_.begin(), velocities_.end(), wpnav_speed);
-            ROS_INFO("Using WPNAV_SPEED: %8.4f as path-following velocities", wpnav_speed);
-            return true;
-          }
-        }
       }
-      auto backup_speed = 4.0;
       velocities_.resize(gcsWaypointNum() - 1);
       std::fill(velocities_.begin(), velocities_.end(), backup_speed);
       ROS_INFO("Using fallback speed: %8.4f for path-following velocities", backup_speed);
@@ -104,33 +63,26 @@ class WaypointManager {
     }
   }
 
-  void computePath() {
+  void computePath(const Eigen::Vector3d &reference_position) {
     waypoints_.clear();
     waypoints_.reserve(wps_.waypoints.size());
     using std::abs;
     for (const auto& it : wps_.waypoints) {
-      if (it.command != 16 || it.frame != 3) {
+      if (it.command != 16 && it.frame != 3) {
         continue;
       }
       if (abs(it.x_lat) < 90 && abs(it.y_long) < 180) {
-        const Eigen::Vector3d lla(it.x_lat, it.y_long, it.z_alt + home_position_.z());
-        auto wp = utils::Geo::CoordsGlobalToLocal(lla, home_position_);
+        const Eigen::Vector3d lla(it.x_lat, it.y_long, it.z_alt + reference_position.z());
+        auto curr_waypoint = utils::Geo::CoordsGlobalToLocal(lla, reference_position);
 
-        waypoints_.push_back(wp);
+        waypoints_.push_back(curr_waypoint);
+
+        if (waypoints_.size() > 1) {
+          const auto& last_waypoint = waypoints_.back();
+          const Eigen::Vector3d heading = curr_waypoint - last_waypoint;
+          headings_.push_back(heading);
+        }
       }
-    }
-  }
-
-  void computeHeadings() {
-    if (headings_.size() > 0) {
-      headings_.clear();
-    }
-
-    for (auto it = waypoints_.begin(); it != waypoints_.end() - 1; ++it) {
-      const auto& curr_waypoint = *it;
-      const auto& next_waypoint = *(it + 1);
-      const Eigen::Vector3d curr_heading = (next_waypoint - curr_waypoint).normalized();
-      headings_.push_back(curr_heading);
     }
   }
 
